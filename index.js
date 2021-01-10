b0f7d886c85dd0f9dbb7235f55725dd5cd2f1893
Nenad Petkovic <root@nenad-petkovic.com>
2021-01-10 20:46:13 +0100

    feat: update project and remove all dependencies.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 /*!
- * node-jsonp - index.js
- * Author:
+ * node-jsonp - main.js
+ * Updated by:
+ * 	  Nenad Petkovic <root@nenad-petkovic.com>
+ * Original author:
  *    ngot <zhuanghengfei@gmail.com>
  */
 
-'use strict'
+'use strict';
 
 /**
  * Module dependencies.
  */
-
-let request = require('request')
-let debug = require('debug')('node-jsonp')
+const http = require('http');
 
 /**
  * JSONP sets up and allows you to execute a JSONP request
@@ -36,95 +36,102 @@ let debug = require('debug')('node-jsonp')
  * })
  */
 
-function JSONP (url, data, method, callback) {
-  function noop () {
-  }
+function JSONP(url, data, method, callback) {
+    function noop() {}
 
-  //  Set the defaults
-  url = url || ''
-  data = data || {}
-  method = method || ''
-  callback = callback || noop
+    //  Set the defaults
+    url = url || '';
+    data = data || {};
+    method = method || '';
+    callback = callback || noop;
 
-  // Gets all the keys that belong
-  // to an object
-  var getKeys = function (obj) {
-    var keys = []
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        keys.push(key)
-      }
+    // Gets all the keys that belong
+    // to an object
+    const getKeys = function (obj) {
+        var keys = [];
+        for (const key of Object.keys(obj)) {
+            keys.push(key);
+        }
+        return keys;
+    };
+
+    // Turn the data object into a query string.
+    // Add check to see if the second parameter is indeed
+    // a data object. If not, keep the default behaviour
+    if (typeof data === 'object') {
+        let queryString = '';
+        const keys = getKeys(data);
+        for (let i = 0; i < keys.length; ++i) {
+            queryString += encodeURIComponent(keys[i]) + '=' + encodeURIComponent(data[keys[i]]);
+            if (i !== keys.length - 1) {
+                queryString += '&';
+            }
+        }
+        url += '?' + queryString;
+    } else if (typeof data === 'function') {
+        method = data;
+        callback = method;
     }
-    return keys
-  }
 
-  // Turn the data object into a query string.
-  // Add check to see if the second parameter is indeed
-  // a data object. If not, keep the default behaviour
-  if (typeof data === 'object') {
-    var queryString = ''
-    var keys = getKeys(data)
-    for (var i = 0; i < keys.length; i++) {
-      queryString += encodeURIComponent(keys[i]) + '=' + encodeURIComponent(data[keys[i]])
-      if (i !== keys.length - 1) {
-        queryString += '&'
-      }
+    // If no method was set and they used the callback param in place of
+    // the method param instead, we say method is callback and set a
+    // default method of "callback"
+    if (typeof method === 'function') {
+        callback = method;
+        method = 'callback';
     }
-    url += '?' + queryString
-  } else if (typeof data === 'function') {
-    method = data
-    callback = method
-  }
 
-  // If no method was set and they used the callback param in place of
-  // the method param instead, we say method is callback and set a
-  // default method of "callback"
-  if (typeof method === 'function') {
-    callback = method
-    method = 'callback'
-  }
+    // Use timestamp + a random factor to account for a lot of requests in a short time
+    // e.g. jsonp1394571775161
+    const generatedFunction = 'jsonp' + Math.round(Date.now() + Math.random() * 1000001);
 
-  // Use timestamp + a random factor to account for a lot of requests in a short time
-  // e.g. jsonp1394571775161
-  var generatedFunction = 'jsonp' + Math.round(Date.now() + Math.random() * 1000001)
+    // Generate the temp JSONP function using the name above
+    // First, call the function the user defined in the callback param [callback(json)]
+    // Then delete the generated function from the global [delete global[generatedFunction]]
+    global[generatedFunction] = function (json) {
+        callback(json);
+        delete global[generatedFunction];
+    };
 
-  // Generate the temp JSONP function using the name above
-  // First, call the function the user defined in the callback param [callback(json)]
-  // Then delete the generated function from the global [delete global[generatedFunction]]
-  global[generatedFunction] = function (json) {
-    callback(json)
-    delete global[generatedFunction]
-  }
-
-  // Check if the user set their own params, and if not add a ? to start a list of params
-  // If in fact they did we add a & to add onto the params
-  // example1: url = http://url.com THEN http://url.com?callback=X
-  // example2: url = http://url.com?example=param THEN http://url.com?example=param&callback=X
-  if (url.indexOf('?') === -1) {
-    url = url + '?'
-  } else {
-    url = url + '&'
-  }
-  url = url + method + '=' + generatedFunction
-  debug('jsonp url: "%s"', url)
-
-  var options = {
-    url: url,
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+    // Check if the user set their own params, and if not add a ? to start a list of params
+    // If in fact they did we add a & to add onto the params
+    // example1: url = http://url.com THEN http://url.com?callback=X
+    // example2: url = http://url.com?example=param THEN http://url.com?example=param&callback=X
+    if (url.indexOf('?') === -1) {
+        url = url + '?';
+    } else {
+        url = url + '&';
     }
-  }
+    url = url + method + '=' + generatedFunction;
 
-  request(options, function (err, res, body) {
-    if (err) throw err
-    else if (res.statusCode === 200) {
-      debug('body: "%j"', body)
+    const options = {
+        headers: {
+            'User-Agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+        },
+    };
 
-      /*eslint-disable no-new-func*/
-      let cb = new Function(body)
-      cb()
-    }
-  })
+    const req = http.get(url, options, res => {
+        res.setEncoding('utf-8');
+
+        let body = {};
+
+        res.on('data', data => {
+            body = data;
+        });
+
+        res.on('end', () => {
+            /*eslint-disable no-new-func*/
+            let cb = new Function(body);
+            cb();
+        });
+    });
+
+    req.on('error', error => {
+        throw error;
+    });
+
+    req.end();
 }
 
-module.exports = JSONP
+module.exports = JSONP;
--- test/index.test.js
+++ test/index.test.js
@@ -1,53 +1,56 @@
 /*!
  * node-jsonp - test/index.test.js
- * Author:
+ * Updated by:
+ *    Nenad Petkovic
+ * Original author:
  *    ngot <zhuanghengfei@gmail.com>
  */
 
-'use strict'
+'use strict';
 
 /**
  * Module dependencies.
  */
 
-let JSONP = require('../')
-let querystring = require('querystring')
+const JSONP = require('../');
+const querystring = require('querystring');
+require('should');
 
 // See http://doc.jsfiddle.net/use/echo.html
-let ENDPOINT = 'http://jsfiddle.net/echo/jsonp/'
+const ENDPOINT = 'http://jsfiddle.net/echo/jsonp/';
 
 describe('index.js', function () {
-  it('querystring jsonp', function (done) {
-    let obj = {
-      a: 'a',
-      b: 'b'
-    }
-    let qs = querystring.encode(obj)
-    JSONP(ENDPOINT + '?' + qs, function (res) {
-      res.should.eql(obj)
-      done()
-    })
-  })
+    it('querystring jsonp', function (done) {
+        let obj = {
+            a: 'a',
+            b: 'b',
+        };
+        let qs = querystring.encode(obj);
+        JSONP(ENDPOINT + '?' + qs, function (res) {
+            res.should.eql(obj);
+            done();
+        });
+    });
 
-  it('param jsonp', function (done) {
-    let obj = {
-      a: 'a',
-      b: 'b'
-    }
-    JSONP(ENDPOINT, obj, function (res) {
-      res.should.eql(obj)
-      done()
-    })
-  })
+    it('param jsonp', function (done) {
+        let obj = {
+            a: 'a',
+            b: 'b',
+        };
+        JSONP(ENDPOINT, obj, function (res) {
+            res.should.eql(obj);
+            done();
+        });
+    });
 
-  it('method', function (done) {
-    let obj = {
-      a: 'a',
-      b: 'b'
-    }
-    JSONP(ENDPOINT, obj, 'callback', function (res) {
-      res.should.eql(obj)
-      done()
-    })
-  })
-})
+    it('method', function (done) {
+        let obj = {
+            a: 'a',
+            b: 'b',
+        };
+        JSONP(ENDPOINT, obj, 'callback', function (res) {
+            res.should.eql(obj);
+            done();
+        });
+    });
+});
